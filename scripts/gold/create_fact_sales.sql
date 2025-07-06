ffc76f8abb1c4fd4e92e30bb2004cfe67bb76122
Huy Nghia Nguyen <116424780+trongnghia2007@users.noreply.github.com>
2025-07-06 12:29:03 +0700

    Add files via upload

---
+++ scripts/gold/create_fact_sales.sql
@@ -0,0 +1,26 @@
+create view gold.fact_sales as
+
+select
+	sd.sls_ord_num as order_number,
+	pr.product_key,
+	cu.customer_key,
+	sd.sls_order_dt as order_date,
+	sd.sls_ship_dt as shipping_date,
+	sd.sls_due_dt as due_date,
+	sd.sls_sales as sales_amount,
+	sd.sls_quantity as quanity,
+	sd.sls_price price
+from silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers cu
+on sd.sls_cust_id = cu.customer_id
+
+
+-- Check if all dimension tables can successfully join to the fact table
+select * from gold.fact_sales f
+left join gold.dim_customers c
+on c.customer_key = f.customer_key
+left join gold.dim_products p
+on p.product_key = f.product_key
+where p.product_key is null
